dba0810b6c9319a527bd7fa526b09c92acba6c31
YashmeetSingh-portfolio <opyash471@gmail.com>
2025-10-07 15:30:59 +0530

    optimisations

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,43 +1,23 @@
 //This file is the layout for the home directory
 
+import ChatProvider from "@/src/providers/ChatProvider";
 import { Stack } from "expo-router";
-import React, { useEffect } from "react";
-import { StreamChat } from "stream-chat";
-import { Chat, OverlayProvider } from "stream-chat-expo";
+import React from "react";
 
 
-const client = StreamChat.getInstance("gdk3kt3bt7ye");//this is a public key so its safe
+
 
 export default function HomeLayout() {
-    useEffect(() => {
-        const connect = async () => {
-            await client.connectUser(
-                {
-                    id: "jlahey",
-                    name: "Jim Lahey",
-                    image: "https://i.imgur.com/fR9Jz14.png",
-                },
-                client.devToken('jlahey'),
-            );
-            // const channel = client.channel("messaging", "the_park", {
-            //     name: "The Park",
-            // });
-            // await channel.watch();
-
-        };
-        connect();
-
-    });
-
+   
     return (
-        <OverlayProvider>
-            <Chat client={client}>
+        
+                <ChatProvider>
 
-            <Stack >
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                </Stack>
+                    <Stack >
+                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                    </Stack>
+                </ChatProvider>
 
-            </Chat>
-        </OverlayProvider>
+            
     );
 }
