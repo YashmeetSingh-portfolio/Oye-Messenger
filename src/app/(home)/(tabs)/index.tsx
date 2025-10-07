e24c3c9983dbdd1c01b230ae1bff233f4e1e1792
YashmeetSingh-portfolio <opyash471@gmail.com>
2025-10-07 14:54:59 +0530

    setup stream Chat UI

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,6 +1,15 @@
-import { Text } from "react-native";
+import { router } from 'expo-router';
+import React from 'react';
+import { ChannelList } from 'stream-chat-expo';
+
 
 export default function MainTabScreen() {
-    return <Text>Main Tab Screen</Text>;
+    
+    const [channel, setChannel] = React.useState();
+    
+    return (
+      
+        <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)}  />
+    );
 
 }
--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,9 +1,43 @@
 //This file is the layout for the home directory
 
-import { Slot } from "expo-router";
-import React from "react";
+import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat, OverlayProvider } from "stream-chat-expo";
 
+
+const client = StreamChat.getInstance("gdk3kt3bt7ye");//this is a public key so its safe
 
 export default function HomeLayout() {
-    return <Slot/>;
+    useEffect(() => {
+        const connect = async () => {
+            await client.connectUser(
+                {
+                    id: "jlahey",
+                    name: "Jim Lahey",
+                    image: "https://i.imgur.com/fR9Jz14.png",
+                },
+                client.devToken('jlahey'),
+            );
+            // const channel = client.channel("messaging", "the_park", {
+            //     name: "The Park",
+            // });
+            // await channel.watch();
+
+        };
+        connect();
+
+    });
+
+    return (
+        <OverlayProvider>
+            <Chat client={client}>
+
+            <Stack >
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                </Stack>
+
+            </Chat>
+        </OverlayProvider>
+    );
 }
