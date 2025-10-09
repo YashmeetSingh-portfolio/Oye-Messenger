e101c91bd51ed9986bcbe8ed66fa94c963c2acde
YashmeetSingh-portfolio <opyash471@gmail.com>
2025-10-09 10:15:54 +0530

    Start chatting with a user

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,18 +1,33 @@
+import { router } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text } from 'react-native';
+import { useChatContext } from 'stream-chat-expo';
+import { useAuth } from '../providers/AuthProvider';
 
 type User = {
-  id: string;
-  full_name: string;
-  avatar_url?: string | null;
+    id: string;
+    full_name: string;
+    avatar_url?: string | null;
 };
 
+
 const UserListItem = ({ user }: { user: User }) => {
-  return (
-    <View style={{ padding: 15, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
-      <Text style={{ fontWeight: '600' }}>{user.full_name}</Text>
-    </View>
-  );
+
+    const {user: me} = useAuth(); 
+    const { client } = useChatContext();
+    const onPress = async () => {
+        //start chat with user
+        const channel = client.channel('messaging', {
+            members: [me?.id!, user.id],
+    });
+    await channel.watch();
+    router.replace(`/(home)/channel/${channel.cid}`);
+}
+    return (
+        <Pressable style={{ padding: 15, borderBottomWidth: 1, borderBottomColor: '#ccc' }} onPress={onPress}>
+            <Text style={{ fontWeight: '600' }}>{user.full_name}</Text>
+        </Pressable>
+    );
 };
 
 export default UserListItem;
